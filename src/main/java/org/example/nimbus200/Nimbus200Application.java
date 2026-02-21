0f570220d98ac20db8a63b6674203528b5767304
Ramesh <ramesh200212@gmail.com>
2026-02-21 15:38:29 +0530

    Initial setup

---
+++ src/main/java/org/example/nimbus200/Nimbus200Application.java
@@ -0,0 +1,13 @@
+package org.example.nimbus200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nimbus200Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nimbus200Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nimbus200/Nimbus200ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nimbus200;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nimbus200ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
